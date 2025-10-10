4d4a0343f8811a614f0c2dfbc749f3cf91110300
farshad-1988 <96774694+farshad-1988@users.noreply.github.com>
2025-10-11 01:26:10 +0330

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+//Next.js automatically handles CSS at runtime, but TypeScript complains because it doesn’t know the type.
+// create a file named global.d.ts in your project root and add accepted modules for this TS app
+// like declare module "*.css" or declare module "*.jpg" ....
+import "./globals.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "../theme";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  //only download the characters we need for better performance
+  subsets: ["latin"],
+  //is related to font-display when font is loading, swap is good for performance(prevent Flash of Invisible Text) and very fast Flash of Unstyled Text
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+// export const metadata: Metadata = {
+//   title: "SHOP APP",
+//   description: "a simple shop app built with nextjs15 and material ui",
+// };
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable}`}>
+        <AppRouterCacheProvider options={{ key: "css" }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+import SearchIcon from "@mui/icons-material/Search";
+import LoginIcon from "@mui/icons-material/Login";
+// feature/: For developing new features,
+// bugfix/: To fix bugs in the code. Often created associated to an issue.
+// hotfix/: To fix critical bugs in the production.
+// release/: To prepare a new release, typically used to do tasks such as last touches and revisions.
+// docs/: Used to write, modify or correct documentation.
+// For example, feature/new-feature or release/version-1.0.0.
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import {
+  AppBar,
+  Badge,
+  Button,
+  Card,
+  IconButton,
+  InputBase,
+  Paper,
+} from "@mui/material";
+// <type>[optional scope]: <description>
+//[optional body]
+//[optional footer(s)]
+// fix: a commit of the type fix patches a bug in your codebase (this correlates with PATCH in Semantic Versioning).
+// feat: a commit of the type feat introduces a new feature to the codebase (this correlates with MINOR in Semantic Versioning).
+// BREAKING CHANGE: a commit that has a footer BREAKING CHANGE:, or appends a ! after the type/scope, introduces a breaking API change (correlating with MAJOR in Semantic Versioning). A BREAKING CHANGE can be part of commits of any type.
+// types other than fix: and feat: are allowed, for example @commitlint/config-conventional (based on the Angular convention) recommends build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
+// footers other than BREAKING CHANGE: <description> may be provided and follow a convention similar to git trailer format.
+
+export default function Home() {
+  // const HeaderBar = styled(AppBar)``;
+
+  return (
+    <>
+      <AppBar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+        }}
+        position="static"
+      >
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            maxWidth: "1200px",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "10px",
+          }}
+        >
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: 300,
+            }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search For Products"
+              inputProps={{ "aria-label": "search Products" }}
+            />
+            <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+          <Card
+            sx={{
+              boxShadow: "none",
+              display: "flex",
+              flexDirection: "row",
+              gap: "20px",
+              alignItems: "center",
+            }}
+          >
+            <Badge badgeContent={0} color="primary">
+              <ShoppingCartIcon />
+            </Badge>
+
+            <Button
+              sx={{
+                border: "1px solid #999",
+                display: "flex",
+                flexDirection: "row",
+                gap: "10px",
+                alignItems: "center",
+              }}
+            >
+              Login <LoginIcon />
+            </Button>
+          </Card>
+        </Card>
+      </AppBar>
+      <main>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+      <footer>
+        <p>&copy; 2024 SHOP APP</p>
+      </footer>
+    </>
+  );
+}
